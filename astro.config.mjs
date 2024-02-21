75b3ab2aef264367f9042b177a34cb9c4715f80b
vclemenzi <vclemenzi@users.noreply.github.com>
2024-02-21 18:23:15 +0100

    chore: cloduflare pages adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,31 +1,32 @@
 import { defineConfig } from 'astro/config';
 import starlight from '@astrojs/starlight';
 
+import cloudflare from "@astrojs/cloudflare";
+
 // https://astro.build/config
 export default defineConfig({
-  integrations: [
-    starlight({
-      title: 'GitArchived',
-      logo: {
-        src: './src/assets/gitarchived.png',
-        replacesTitle: true,
-      },
-      customCss: [
-        './src/styles/custom.css'
-      ],
-      social: {
-        github: 'https://github.com/gitarchived',
-      },
-      sidebar: [
-        {
-          label: 'Reference',
-          autogenerate: { directory: 'reference' },
-        },
-        {
-          label: 'Information',
-          autogenerate: { directory: 'information' },
-        },
-      ],
-    }),
-  ],
+  integrations: [starlight({
+    title: 'GitArchived',
+    logo: {
+      src: './src/assets/gitarchived.png',
+      replacesTitle: true
+    },
+    customCss: ['./src/styles/custom.css'],
+    social: {
+      github: 'https://github.com/gitarchived'
+    },
+    sidebar: [{
+      label: 'Reference',
+      autogenerate: {
+        directory: 'reference'
+      }
+    }, {
+      label: 'Information',
+      autogenerate: {
+        directory: 'information'
+      }
+    }]
+  })],
+  output: "server",
+  adapter: cloudflare({ mode: "directory" }),
 });
