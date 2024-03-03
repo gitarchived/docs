ec88a33fb71c97ab2de5213b689a809e22e6a818
vclemenzi <vclemenzi@users.noreply.github.com>
2024-03-03 14:28:50 +0100

    fix: website favicon

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,6 +7,7 @@ import cloudflare from "@astrojs/cloudflare";
 export default defineConfig({
   integrations: [starlight({
     title: 'GitArchived',
+    favicon: './public/favicon.png',
     logo: {
       src: './src/assets/gitarchived.png',
       replacesTitle: true
